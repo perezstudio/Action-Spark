67a8d1b127a225dea12b634833ce7e7aad067d8a
Kevin Perez <kp952547@gmail.com>
2024-02-10 11:35:36 -0700

    Delete unused settings file and fix formatting in DivBlock.svelte

--- src/lib/blocks/logic/ForEachBlock.svelte
+++ src/lib/blocks/logic/ForEachBlock.svelte
@@ -1,11 +1,14 @@
-<!-- DivBlock.svelte -->
-<script>
+<!-- ForEachBlock.svelte -->
+<script lang="ts">
   import ElementBase from "$lib/blocks/ElementBase.svelte";
   export let customClasses = ""; // For additional styling
+  export let items: any[] = [];
 </script>
 
 <ElementBase customClasses={`bg-white shadow-md ${customClasses}`}>
-  <div>
-    <slot />
+  <div class="foreach-block">
+    {#each items as item}
+      <slot name="item" {item}></slot>
+    {/each}
   </div>
 </ElementBase>
